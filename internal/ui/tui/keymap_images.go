cf93b562e9325b86eaf535b098ab52b5327587a1
yuasalily <yuasalily1011@gmail.com>
2026-01-11 15:55:41 +0000

    イメージの削除モード実装

--- internal/ui/tui/keymap_images.go
+++ internal/ui/tui/keymap_images.go
@@ -4,8 +4,15 @@ import "github.com/charmbracelet/bubbles/key"
 
 type imagesKeyMap struct {
 	Refresh key.Binding
+
+	// Normal mode
+	DeleteSingle    key.Binding
+	EnterDeleteMode key.Binding
+
+	// Delete mode
 	Select  key.Binding
-	Delete  key.Binding
+	Execute key.Binding
+	Exit    key.Binding
 }
 
 func newImagesKeyMap() imagesKeyMap {
@@ -14,13 +21,25 @@ func newImagesKeyMap() imagesKeyMap {
 			key.WithKeys("r"),
 			key.WithHelp("r", "refresh"),
 		),
+		DeleteSingle: key.NewBinding(
+			key.WithKeys("d"),
+			key.WithHelp("d", "delete"),
+		),
+		EnterDeleteMode: key.NewBinding(
+			key.WithKeys("D"),
+			key.WithHelp("D", "delete mode"),
+		),
 		Select: key.NewBinding(
 			key.WithKeys(" ", "space"),
 			key.WithHelp("space", "select"),
 		),
-		Delete: key.NewBinding(
-			key.WithKeys("d"),
-			key.WithHelp("d", "delete"),
+		Execute: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "execute"),
+		),
+		Exit: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "exit"),
 		),
 	}
 }
